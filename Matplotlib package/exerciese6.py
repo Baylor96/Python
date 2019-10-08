a14aa075bdc5cc6a7d2c592dde726183093b7bc6
Geo-Albert <271329921@qq.com>
2019-10-08 15:58:58 +0800

    Initial commit

---
+++ Matplotlib package/exerciese6.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+n = 1024
+X = np.random.normal(0,1,n)
+Y = np.random.normal(0,1,n)
+T = np.arctan2(Y,X)
+
+plt.scatter(X,Y,s=75,c=T,alpha=0.5)
+
+plt.xlim(-1.5,1.5)
+plt.ylim(-1.5,1.5)
+
+# Hidden coordinate axis label
+plt.xticks(())
+plt.yticks(())
+
+plt.show()
