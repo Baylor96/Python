c0ce221afc6c80e795ed5fd561b970e95355c08e
Geo-Albert <271329921@qq.com>
2019-10-23 16:10:42 +0800

    modified

--- lxml package/Make_xml.py
+++ lxml package/Make_xml.py
@@ -1,34 +1,32 @@
 from lxml import etree, objectify
+import datetime
 
-E = objectify.ElementMaker(annotate=False)
-anno_tree = E.annotation(
-    E.folder('ljy'),
-    E.filename("ljy.jpg"),
-    E.source(
-        E.database('lala'),
-        E.annotation('lala'),
-        E.image('lala'),
-        E.url("http://test.jpg")
-    ),
-    E.size(
-        E.width(800),
-        E.height(600),
-        E.depth(3)
-    ),
-    E.segmented(0),
-)
+FDSNStationXML = etree.Element("FDSNStationXML", xsi='http://www.w3.org/2001/XMLSchema-instance')
+etree.SubElement(FDSNStationXML, "Source").text = "ljy"
+etree.SubElement(FDSNStationXML, "Sender").text = "ljy"
+etree.SubElement(FDSNStationXML, 'Module').text = 'ljy'
+etree.SubElement(FDSNStationXML, 'ModuleURI').text = 'https://github.com/Junyi-Li96/LASIF-script'
+etree.SubElement(FDSNStationXML, 'Created').text = str(datetime.datetime.now().replace(microsecond=0).isoformat())
+etree.SubElement(FDSNStationXML, 'Network', code = 'LA', startDate='1970-01-01T00:00:00', endDate="2599-12-31T23:59:59", restrictedStatus="open")
 
-E2 = objectify.ElementMaker(annotate=False)
-anno_tree2 = E2.object(
-    E.name("person"),
-    E.bndbox(
-        E.xmin(100),
-        E.ymin(200),
-        E.xmax(300),
-        E.ymax(400)
-    ),
-    E.difficult(0)
-)
-anno_tree.append(anno_tree2)
+# source = etree.SubElement(annotation, "source")
+# etree.SubElement(source, "database").text = "COCO"
+# etree.SubElement(source, "annotation").text = "COCO"
+# etree.SubElement(source, "image").text = "COCO"
+# etree.SubElement(source, "url").text = "http://test.jpg"
+# size = etree.SubElement(annotation, "size")
+# etree.SubElement(size, "width").text ='800' # 必须用string
+# etree.SubElement(size, "height").text = '600'
+# etree.SubElement(size, "depth").text = '3'
+# etree.SubElement(annotation, "segmented").text = '0'
+# key_object = etree.SubElement(annotation, "object")
+# etree.SubElement(key_object, "name").text = 'person'
+# bndbox = etree.SubElement(key_object, "bndbox")
+# etree.SubElement(bndbox, "xmin").text = str(100)
+# etree.SubElement(bndbox, "ymin").text = str(200)
+# etree.SubElement(bndbox, "xmax").text = str(300)
+# etree.SubElement(bndbox, "ymax").text = str(400)
+# etree.SubElement(key_object, "difficult").text = '0'
 
-etree.ElementTree(anno_tree).write("text.xml", pretty_print=True)
+doc = etree.ElementTree(FDSNStationXML)
+doc.write("test.xml", pretty_print=True)
