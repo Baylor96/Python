1d95642c71e000f3e4a562ae471c5f4a03626cbb
Geo-Albert <271329921@qq.com>
2019-11-18 16:58:39 +0800

    Initial commit

---
+++ Obspy package/Envelopes.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import obspy.signal
+
+st = obspy.read('./sacdata/CA.1563A.01.HHE.sac')
+data = st[0].data
+npts = st[0].stats.npts
+samprate = st[0].stats.sampling_rate
+
+st_filt = st.copy()
+st_filt.filter('bandpass', freqmin=1, freqmax=10, corners=2, zerophase=True)
+data_envelope = obspy.signal.filter.envelope(st_filt[0].data)
+t = np.arange(0, npts / samprate, 1 / samprate)
+
+plt.plot(t, st_filt[0].data, 'k')
+plt.plot(t, data_envelope, 'r')
+plt.title(st[0].stats.starttime)
+plt.ylabel('Filtered Data w/ Envelope')
+plt.xlabel('Time [s]')
+plt.show()
