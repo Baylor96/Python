1671a6493173c96cd2425f0c11af21251270cdf9
Geo-Albert <271329921@qq.com>
2019-11-19 19:51:21 +0800

    mobilization

--- Obspy package/Spectrograms.py
+++ Obspy package/Spectrograms.py
@@ -5,5 +5,7 @@ PATH = './sacdata'
 os.chdir(PATH)
 for each_sac in os.listdir(os.curdir):
     sac = obspy.read(each_sac)
+    tr = sac[0]
+    samprate = tr.stats.sampling_rate
     name = os.path.splitext(each_sac)[0]
-    sac.spectrogram(log=True, title=name)
+    sac.spectrogram(log=True, title=name, clip=[0,0.5], outfile=name + '.png', fmt='png')
