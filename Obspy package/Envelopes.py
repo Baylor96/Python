e9f35b050c9bb6585ac47990e3c15074f0414937
Geo-Albert <271329921@qq.com>
2019-11-27 15:44:27 +0800

    mobilization

--- Obspy package/Envelopes.py
+++ Obspy package/Envelopes.py
@@ -2,13 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import obspy.signal
 
-st = obspy.read('./sacdata/CA.1563A.01.HHE.sac')
+st = obspy.read('./sacdata/CA.WT74.01.HHE.sac')
 data = st[0].data
 npts = st[0].stats.npts
 samprate = st[0].stats.sampling_rate
+print(npts,samprate,st[0].stats.delta)
 
 st_filt = st.copy()
-st_filt.filter('bandpass', freqmin=1, freqmax=10, corners=2, zerophase=True)
+st_filt.filter('bandpass', freqmin=10, freqmax=20, corners=2, zerophase=True)
 data_envelope = obspy.signal.filter.envelope(st_filt[0].data)
 t = np.arange(0, npts / samprate, 1 / samprate)
 
