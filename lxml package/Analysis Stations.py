425cdcb133c58389c48b2f96dd02c90a6176942a
Geo-Albert <271329921@qq.com>
2019-12-19 12:41:38 +0800

    fix bug

--- lxml package/Analysis Stations.py
+++ lxml package/Analysis Stations.py
@@ -4,7 +4,7 @@ from lxml import etree
 # Stations = etree.parse("lagou.html", parser=Stations_parser)
 
 Stations = etree.parse("Station.html")
-# print(etree.tostring(Stations, encoding='utf-8').decode('utf-8'))
-Channals = Stations.xpath("//Site")
-for Channal in Channals:
-    print(etree.tostring(Channal, encoding='utf-8').decode('utf-8'))
+print(etree.tostring(Stations, encoding='utf-8').decode('utf-8'))
+# Channals = Stations.xpath("//Site")
+# for Channal in Channals:
+#     print(etree.tostring(Channal, encoding='utf-8').decode('utf-8'))
