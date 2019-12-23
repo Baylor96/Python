523f6c90a97d34f287552021344c7f1ce4c38bbf
Geo-Albert <271329921@qq.com>
2019-12-23 11:37:58 +0800

    Initial commit

--- Tensorflow package/Dropout.py
+++ Tensorflow package/Dropout.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-import numpy as np
 import matplotlib.pyplot as plt
 
 (train_image, train_label), (test_image, test_label) = tf.keras.datasets.fashion_mnist.load_data()
@@ -13,25 +12,24 @@ test_image = test_image/255
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
 model.add(tf.keras.layers.Dense(128, activation='relu'))
+model.add(tf.keras.layers.Dropout(0.5))
 model.add(tf.keras.layers.Dense(128, activation='relu'))
+model.add(tf.keras.layers.Dropout(0.5))
 model.add(tf.keras.layers.Dense(128, activation='relu'))
+model.add(tf.keras.layers.Dropout(0.5))
 model.add(tf.keras.layers.Dense(10, activation='softmax'))
 
-# model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.01), loss='categorical_crossentropy', metrics=['acc'])
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
-histroy = model.fit(train_image, train_label_onehot, epochs=10, validation_data=(test_image, test_label_onehot))
+history = model.fit(train_image, train_label_onehot, epochs=10, validation_data=(test_image, test_label_onehot))
 
-print(histroy.history.keys())
+print(history.history.keys())
 
-figure1 = plt.figure()
-plt.plot(histroy.epoch, histroy.history.get('loss'), label='loss')
-plt.plot(histroy.epoch, histroy.history.get('val_loss'), label='val_loss')
+fig1 = plt.figure()
+plt.plot(history.epoch, history.history.get('acc'), label='acc')
+plt.plot(history.epoch, history.history.get('val_acc'), label='val_acc')
 plt.legend()
-figure2 = plt.figure()
-plt.plot(histroy.epoch, histroy.history.get('acc'), label='acc')
-plt.plot(histroy.epoch, histroy.history.get('val_acc'), label='val_acc')
+fig2 = plt.figure()
+plt.plot(history.epoch, history.history.get('loss'), label='loss')
+plt.plot(history.epoch, history.history.get('val_loss'), label='val_loss')
 plt.legend()
 plt.show()
-
-# predict = model.predict(test_image)
-# print(predict[0],np.argmax(predict[0]))
