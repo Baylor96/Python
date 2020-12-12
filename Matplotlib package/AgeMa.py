d22b7a61560f67f4d68f427ff81da838331c78a6
mac <271329921@qq.com>
2020-12-12 17:08:14 +0800

    optimization

--- Matplotlib package/AgeMa.py
+++ Matplotlib package/AgeMa.py
@@ -1,5 +1,6 @@
 import pandas
 import matplotlib.pyplot as plt
+import numpy as np
 import os
 
 data_path = os.path.join(os.path.dirname(__file__), 'data.xls')
@@ -10,7 +11,15 @@ print(data.columns)
 x = data['d18O   ']
 y = data['Best_AgeMa']
 
-plt.xlabel('O')
+figure = plt.figure(figsize=(5, 10))
+
+ax = plt.gca()
+ax.xaxis.set_ticks_position('top')
+ax.yaxis.set_ticks_position('left')
+ax.invert_xaxis()
+ax.invert_yaxis()
+
+plt.xlabel('O18')
 plt.ylabel('Age')
-plt.scatter(x, y)
+plt.scatter(x, y, c=np.random.random(10801))
 plt.show()
