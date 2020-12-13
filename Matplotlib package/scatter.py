308949378215a4d6a5f195b78a607a72a2482d2f
Geo-Albert <271329921@qq.com>
2020-12-13 09:57:10 +0800

    update code

--- Matplotlib package/scatter.py
+++ Matplotlib package/scatter.py
@@ -1,20 +1,33 @@
-from matplotlib import pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
+import matplotlib.pyplot as plt
 
-#定义坐标轴
-fig4 = plt.figure()
-ax4 = plt.axes(projection='3d')
 
-#生成三维数据
-xx = np.random.random(20)*10-5   #取100个随机数，范围在5~5之间
-yy = np.random.random(20)*10-5
-X, Y = np.meshgrid(xx, yy)
-Z = np.sin(np.sqrt(X**2+Y**2))
+def plot_scatter(x1, y1, x2, y2):
+    plt.rcParams['font.sans-serif'] = ['SimHei']
+    plt.rcParams['axes.unicode_minus'] = False
 
-#作图
-ax4.scatter(X,Y,Z,alpha=0.3,c=np.random.random(400),s=np.random.randint(10,20, size=(20, 40)))     #生成散点.利用c控制颜色序列,s控制大小
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.xlim(xmax=9, xmin=0)
+    plt.ylim(ymax=9, ymin=0)
+    plt.grid(linestyle="--")
 
-#设定显示范围
+    colors1 = '#00CED1'
+    colors2 = '#DC143C'
+    area = np.pi * 4 ** 2
 
-plt.show()
+    plt.scatter(x1, y1, s=area, c=colors1, alpha=0.4, label='陈健龙1')
+    plt.scatter(x2, y2, s=area, c=colors2, alpha=0.4, label='陈健龙2')
+    plt.plot([0, 9.5], [9.5, 0], linewidth='0.5', color='#000000')
+    plt.legend(loc=2)
+    plt.savefig('12345.png', dpi=300)
+    plt.show()
+
+
+if __name__ == '__main__':
+    x1 = np.random.normal(2, 1.2, 300)
+    y1 = np.random.normal(2, 1.2, 300)
+    x2 = np.random.normal(7.5, 1.2, 300)
+    y2 = np.random.normal(7.5, 1.2, 300)
+
+    plot_scatter(x1, y1, x2, y2)
